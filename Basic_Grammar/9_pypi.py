8557d5311c619a43e1f971f1b0f477e5b1a0c345
jy72700-beep <jy72700@gmail.com>
2026-04-10 15:57:08 +0900

    Add files via upload

---
+++ Basic_Grammar/9_pypi.py
@@ -0,0 +1,15 @@
+'''
+import random
+print('random.random()',random.random())
+#pip pandas install 
+#pandaas를 설치하는 명령어
+#python3 -m pip install pandas 이렇게 하라고 하심
+'''
+#분석하고 싶은 파일을 csv로 다운받아서 pandas로 분석하기
+'''
+import pandas as pd
+data = pd.read_csv('data.csv')
+print(data)
+'''
+#중요한것은 pandas와 같은 라이브러리를 이용하는 것을 아는 것
+
