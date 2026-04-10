e38cd491d80b1e709f86b869fb2f2d3131134738
jy72700-beep <jy72700@gmail.com>
2026-04-10 15:25:42 +0900

    Add files via upload
    debugging

---
+++ Basic_Grammar/7_debug.py
@@ -0,0 +1,43 @@
+'''
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+#print('a',a) #debugging을 위해 print문을 이용해 변수의 값을 출력
+#a = 8 know that this is a bug
+a = 1 #debugging을 위해 변수 a의 값을 1로 바꿔줌
+#print('a',a)
+h = 9
+i = 8
+j = 7
+k = 6
+l = 5
+m = 4
+n = 3
+o = 2
+p = 1
+print(a)
+'''
+
+#let's learn about debugger
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+a = 2
+#print('a',a)
+h = 9
+i = 8
+j = 7
+k = 6
+l = 5
+m = 4
+n = 3
+o = 2
+p = 1
