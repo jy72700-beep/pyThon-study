d5cbae50b9fc42e1e90f3e66822b996d4431b09d
jy72700-beep <jy72700@gmail.com>
2026-04-10 14:34:42 +0900

    Add files via upload
    살짝 수정

--- Basic_Grammar/6_varaible.py
+++ Basic_Grammar/6_varaible.py
@@ -1,6 +1,8 @@
 name = "sori"
 #print('hi, sori....bye, sori.')
 #print('hi, '+ name + '....bye, ' + name+'.')
+#print(f'hi, {name}....bye, {name}.')
+
 message = 'hi, '+ name + '....bye, ' + name+'.'
 print(message)
 
